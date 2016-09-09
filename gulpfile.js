c3cf439c3c066e3a739c8a5f89ebe7ade13c1ad0
Kjell Bublitz <kjell.bublitz@mediacode.de>
2016-09-10 01:35:14 +0200

    migrated release task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,36 @@
 'use strict';
-
+var gulp = require('gulp');
 var elixir = require('laravel-elixir');
 elixir.config.assetsPath = 'frontend';
 elixir.config.publicPath = 'public';
 
-elixir(function(mix) {
-
-  mix.browserify('paperphp.js');
-  
-  mix.sass('paperphp.scss');
-  
+elixir.extend('release', function(name) {
+  var paths = [
+    'cache',
+    'frontend/**',
+    'pages/**',
+    'public/.htaccess',
+    'public/**',
+    'src/**',
+    'templates/**',
+    'vendor/**',
+    '.htaccess',
+    'config.json.default',
+    'package.json',
+    'README.md'
+  ];
+  new elixir.Task('release', function(){
+    var archiver = require('gulp-archiver');
+    this.recordStep('Creating "paperphp.zip"');
+    gulp.src(paths, { base: '.' })
+      .pipe(archiver(name))
+      .pipe(gulp.dest('./release/'));
+  });
 });
+
+elixir(function(mix) {
+  mix.browserify('paperphp.js');
+  mix.sass('paperphp.scss');
+  mix.release('paperphp.zip');
+});
+
